ac0232d83d6f9061b41c048d67169960efdd6f64
Jonatan Brian Mild <jonatan.mild@southworks.com>
2020-04-15 16:41:55 -0300

    add config file and read of configuration

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD:app.js
 const fastify = require('fastify')({ logger: true });
 const port = process.argv[2];
 const routes = require('./routes/task.routes');
@@ -24,4 +25,33 @@ const start = async () => {
     }
 }
 
+=======
+const fastify = require('fastify')({ logger: true });
+const port = process.argv[2];
+const routes = require('./routes/task.routes');
+const mongoose = require('mongoose')
+
+require('dotenv-safe').config();
+
+// Connect to DB
+mongoose.connect(process.env.CONNECTION_STRING, {
+useNewUrlParser: true
+}).then(() => console.log("MongoDB connected…"))
+.catch(err => console.log(err));
+
+routes.forEach((route, index)=> {
+    fastify.route(route);
+})
+
+const start = async () => {
+    try {
+        await fastify.listen(port);
+        fastify.log.info(`server listening on ${fastify.server.address().port}`);
+    } catch (err) {
+        fastify.log.error(err);
+        process.exit(1);
+    }
+}
+
+>>>>>>> a739f6c... add config file and read of configuration:index.js
 start();
