319450cdf4afb3942e5151246ade00f8ec6410fd
Jonatan Brian Mild <jonatan.mild@southworks.com>
2020-04-15 16:48:31 -0300

    fix merge conflict

--- app.js
+++ app.js
@@ -1,31 +1,3 @@
-<<<<<<< HEAD:app.js
-const fastify = require('fastify')({ logger: true });
-const port = process.argv[2];
-const routes = require('./routes/task.routes');
-
-const mongoose = require('mongoose')
-
-// Connect to DB
-mongoose.connect('mongodb+srv://dbUser:74PvvgbdFPQdNEi6@demo-fduq8.gcp.mongodb.net/test?retryWrites=true&w=majority', {
-useNewUrlParser: true
-}).then(() => console.log("MongoDB connected…"))
-.catch(err => console.log(err));
-
-routes.forEach((route, index)=> {
-    fastify.route(route);
-})
-
-const start = async () => {
-    try {
-        await fastify.listen(port);
-        fastify.log.info(`server listening on ${fastify.server.address().port}`);
-    } catch (err) {
-        fastify.log.error(err);
-        process.exit(1);
-    }
-}
-
-=======
 const fastify = require('fastify')({ logger: true });
 const port = process.argv[2];
 const routes = require('./routes/task.routes');
@@ -53,5 +25,4 @@ const start = async () => {
     }
 }
 
->>>>>>> a739f6c... add config file and read of configuration:index.js
 start();
