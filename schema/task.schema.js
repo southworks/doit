d4dc0682e0398a7c8ab956b949d74e0c5084a0eb
Agustin Osatinsky <agustinosatinsky@hotmail.com>
2020-04-16 12:02:36 -0300

    added delete property

--- schema/task.schema.js
+++ schema/task.schema.js
@@ -5,7 +5,8 @@ const mongoose = require('mongoose')
 const taskSchema = new mongoose.Schema({
   name: String,
   is_completed: Boolean,
-  created_at: { type : Date, default: Date.now }
+  created_at: { type : Date, default: Date.now },
+  deleted:  { type : Date, default: 0 }
 })
 
 module.exports = mongoose.model('Task', taskSchema)
